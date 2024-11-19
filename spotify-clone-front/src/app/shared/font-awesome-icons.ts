1d68f74b3fa40abe1ea0982026a9f51c12242105
SonnyHardy <htambeck@gmail.com>
2024-11-19 22:25:30 +0100

    Creation of the Library component

--- spotify-clone-front/src/app/shared/font-awesome-icons.ts
+++ spotify-clone-front/src/app/shared/font-awesome-icons.ts
@@ -1,8 +1,10 @@
 import {IconDefinition} from "@fortawesome/free-regular-svg-icons";
-import {faCirclePlay, faHome, faSearch} from "@fortawesome/free-solid-svg-icons";
+import {faBook, faCirclePlay, faHome, faPlus, faSearch} from "@fortawesome/free-solid-svg-icons";
 
 export const fontAwesomeIcons: IconDefinition[] = [
   faCirclePlay,
   faHome,
-  faSearch
+  faSearch,
+  faBook,
+  faPlus
 ]
