d710cebdac805ad1bffa5f6ca3353fc1639752f1
SonnyHardy <htambeck@gmail.com>
2024-11-25 14:51:56 +0100

    Implementation of the AuthInterceptor

--- spotify-clone-front/src/app/app.config.ts
+++ spotify-clone-front/src/app/app.config.ts
@@ -2,14 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient, withXsrfConfiguration} from "@angular/common/http";
+import {provideHttpClient, withInterceptors, withXsrfConfiguration} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
+import {authInterceptor} from "./service/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
+      withInterceptors([authInterceptor]),
       withXsrfConfiguration({cookieName: 'XSRF-TOKEN', headerName: 'X-XSRF-TOKEN'})
     ),
     provideAnimations()
